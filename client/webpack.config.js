d9915736691fc61d650d39783412c986dd09897e
stanleysq <10691963+stanleysq@users.noreply.github.com>
2017-03-21 16:03:56 +0800

    Set up webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,63 +1,68 @@
-/* eslint comma-dangle: ["error",
-  {"functions": "never", "arrays": "only-multiline", "objects":
-"only-multiline"} ] */
+const webpack = require("webpack")
+const path = require("path")
 
-const webpack = require('webpack');
-const path = require('path');
-
-const devBuild = process.env.NODE_ENV !== 'production';
-const nodeEnv = devBuild ? 'development' : 'production';
+const nodeEnv = process.env.NODE_ENV || "development"
 
 const config = {
   entry: [
-    'es5-shim/es5-shim',
-    'es5-shim/es5-sham',
-    'babel-polyfill',
-    './app/bundles/HelloWorld/startup/registration',
+    "babel-polyfill",
+    "react-hot-loader/patch",
+    "webpack-dev-server/client?http://localhost:3500",
+    "entry"
   ],
 
   output: {
-    filename: 'webpack-bundle.js',
-    path: '../app/assets/webpack',
+    filename: "hmr-bundle.js",
+    publicPath: "http://localhost:3500/"
   },
 
   resolve: {
-    extensions: ['', '.js', '.jsx'],
-    alias: {
-      react: path.resolve('./node_modules/react'),
-      'react-dom': path.resolve('./node_modules/react-dom'),
-    },
+    modules: [path.resolve("./src"), "node_modules"],
+    extensions: [".js"],
   },
+
   plugins: [
     new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify(nodeEnv),
-      },
+      "process.env": { NODE_ENV: JSON.stringify(nodeEnv) }
     }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin()
   ],
+
   module: {
-    loaders: [
+    rules: [
       {
-        test: require.resolve('react'),
-        loader: 'imports?shim=es5-shim/es5-shim&sham=es5-shim/es5-sham',
+        test: /\.js$/,
+        loader: "babel-loader",
+        include: [path.resolve("./src")]
       },
+
       {
-        test: /\.jsx?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-      },
-    ],
+        test: /\.css$/,
+        loader: [
+          "style-loader?sourceMap",
+          "css-loader?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]"
+        ]
+      }
+    ]
   },
-};
 
-module.exports = config;
-
-if (devBuild) {
-  console.log('Webpack dev build for Rails'); // eslint-disable-line no-console
-  module.exports.devtool = 'eval-source-map';
-} else {
-  config.plugins.push(
-    new webpack.optimize.DedupePlugin()
-  );
-  console.log('Webpack production build for Rails'); // eslint-disable-line no-console
+  devServer: {
+    port: 3500,
+    hot: true,
+    stats: {
+      hash: false,
+      version: false,
+      chunks: false
+    }
+  }
 }
+
+if (nodeEnv !== "production") {
+  config.plugins.push(new webpack.NoEmitOnErrorsPlugin())
+  config.devtool = "cheap-module-source-map"
+} else {
+  config.devtool = "eval"
+}
+
+module.exports = config
